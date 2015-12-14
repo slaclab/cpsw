02a52f7ebd4374c83faf94ca2ad7b4d14e2a029f
till straumann <till.straumann@alumni.tu-berlin.de>
2015-12-13 22:46:07 -0800

    - moved free-list to a template
       intend to re-use for chains and maybe other objects....

--- cpsw_buf.cc
+++ cpsw_buf.cc
@@ -6,6 +6,7 @@
 
 #include <cpsw_api_user.h>
 #include <cpsw_error.h>
+#include <cpsw_freelist.h>
 
 #include <stdio.h>
 
@@ -27,30 +28,21 @@ using boost::memory_order_acquire;
 #define NULLBUF   BufImpl( reinterpret_cast<CBufImpl*>(0) )
 #define NULLCHAIN BufChainImpl( reinterpret_cast<CBufChainImpl*>(0) )
 
-class CBufKey {
-private:
-	CBufKey() {};
-	friend class CBufFreeList;
-};
-
-class CBufImpl : public IBuf {
+class CBufImpl : public IBuf, public CFreeListNode<CBufImpl> {
 private:
 	weak_ptr<CBufChainImpl> chain_;
 	BufImpl            next_; 
 	weak_ptr<CBufImpl> prev_;
-	weak_ptr<CBufImpl> self_;
 	unsigned beg_,  end_;
-	uint8_t  data_[3*512 - 2*sizeof(prev_) - sizeof(self_) - 2*sizeof(beg_)];
+	uint8_t  data_[3*512 - 2*sizeof(prev_) - sizeof(CFreeListNode<CBufImpl>) - 2*sizeof(beg_)];
 protected:
 	virtual void     addToChain(BufImpl p, bool);
 	virtual void     delFromChain();
 
 public:
-	CBufImpl(CBufKey k);
+	CBufImpl(CFreeListNodeKey<CBufImpl> k);
 
 	virtual void     addToChain(BufChainImpl c);
-
-	void     setSelf( BufImpl me );
 
 	size_t   getCapacity() { return sizeof(data_);                    }
 	size_t   getSize()     { return end_ - beg_;                      }
@@ -137,16 +129,9 @@ BufChain IBufChain::create()
 }
 
 
-CBufImpl::CBufImpl(CBufKey k)
+CBufImpl::CBufImpl(CFreeListNodeKey<CBufImpl> k)
 : beg_(0), end_(sizeof(data_))
 {
-}
-
-void CBufImpl::setSelf( BufImpl me )
-{
-	if ( ! self_.expired() || me.get() != this ) 
-		throw InternalError("Self pointer already set or not matching 'this'");
-	self_ = me;
 }
 
 void CBufImpl::setSize(size_t s)
@@ -211,12 +196,12 @@ BufChainImpl pc = p->getChain();
 		if ( c ) {
 			if ( before ) {
 				if ( (p == c->getHeadImpl()) ) {
-					BufImpl me = BufImpl( self_ );
+					BufImpl me = getSelf();
 					c->setHead( me );
 				}
 			} else {
 				if ( (p == c->getTailImpl()) ) {
-					BufImpl me = BufImpl( self_ );
+					BufImpl me = getSelf();
 					c->setTail( me );
 				}
 			}
@@ -229,7 +214,7 @@ BufChainImpl pc = p->getChain();
 void CBufImpl::addToChain(BufChainImpl c)
 {
 BufImpl  p,h,t;
-BufImpl  me = BufImpl(self_);
+BufImpl  me = getSelf();
 size_t   s = 0;
 unsigned l = 0;
 	if ( c->getHead() || c->getTail() )
@@ -260,7 +245,7 @@ void CBufImpl::delFromChain()
 {
 BufChainImpl c  = getChain();
 	if ( c ) {
-		BufImpl me = BufImpl( self_ );
+		BufImpl me = getSelf();
 		if ( c->getHeadImpl() == me ) {
 			c->setHead( c->getHeadImpl()->getNextImpl() );
 		}
@@ -276,7 +261,7 @@ BufChainImpl c  = getChain();
 void CBufImpl::after(Buf p)
 {
 BufImpl pi = static_pointer_cast<BufImpl::element_type>(p);
-BufImpl me = BufImpl(self_);
+BufImpl me = getSelf();
 
 	if ( pi && pi != me ) {
 
@@ -297,7 +282,7 @@ BufImpl me = BufImpl(self_);
 void CBufImpl::before(Buf p)
 {
 BufImpl pi = static_pointer_cast<BufImpl::element_type>(p);
-BufImpl me = BufImpl(self_);
+BufImpl me = getSelf();
 
 	if ( pi && pi != me ) {
 		if ( getNextImpl() || getPrevImpl() )
@@ -337,112 +322,14 @@ void CBufImpl::split()
 	}
 }
 
-class CBufAlloc : public std::allocator<CBufImpl> {
-private:
-	atomic<unsigned int> *nAlloc_p_;
-	atomic<unsigned int> *nFree_p_;
-public:
-	CBufAlloc( atomic<unsigned int> *nAlloc_p, atomic<unsigned int> *nFree_p )
-	:nAlloc_p_(nAlloc_p), nFree_p_(nFree_p)
-	{
-	}
-
-	CBufImpl *allocate(size_t n)
-	{
-		nAlloc_p_->fetch_add(n, memory_order_release);
-#ifndef BOOST_LOCKFREE_FREELIST_INIT_RUNS_DTOR
-		nFree_p_->fetch_add(n, memory_order_release);
-#endif
-		return std::allocator<CBufImpl>::allocate( n );
-	}
-};
-
-// g++ (4.8.4) seems to get confused (as I do) by all the template stuff.
-// These typedefs seem to work around it; have no idea why (found by trial + error).
-typedef CBufAlloc                           ALL;
-typedef freelist_stack<CBufImpl, CBufAlloc> FreeListBase;
-
-class CBufFreeList;
-
-class DTOR {
-private:
-	CBufFreeList *fl_;
-public:
-	DTOR(CBufFreeList *fl) : fl_ (fl) {}
-	void operator()(CBufImpl *o);
-};
-
-
-class CBufFreeList : public FreeListBase
-{
-private:
-	atomic<unsigned int> nAlloc_;
-	atomic<unsigned int> nFree_;
-public:
-	CBufFreeList(int n = 0)
-	:FreeListBase( ALL( (nAlloc_=0, &nAlloc_ ), (nFree_ = 0, &nFree_) ) , n)
-	{}
-
-	unsigned int getNumAlloced()
-	{
-		return nAlloc_.load( memory_order_acquire );
-	}
-
-	unsigned int getNumFree()
-	{
-		return nFree_.load( memory_order_acquire );
-	}
-
-	unsigned int getNumInUse()
-	{
-		// can't atomically read both -- since this is a diagnostic
-		// we don't care about marginal race conditions here.
-		unsigned int rval = getNumAlloced();
-		rval -= getNumFree();
-		return rval;
-	}
-
-	CBufImpl *construct(CBufKey k)
-	{
-		const bool ThreadSafe = true;
-		const bool Bounded    = false;
-		CBufImpl *rval = FreeListBase::construct<ThreadSafe,Bounded>( k );
-		if ( rval )
-			nFree_.fetch_sub( 1, memory_order_release );
-		return rval;
-	}
-
-	void destruct(CBufImpl *o)
-	{
-		const bool ThreadSafe = true;
-		nFree_.fetch_add( 1, memory_order_release );
-		FreeListBase::destruct<ThreadSafe>( o );
-	}
-
-	BufImpl getBuf()
-	{
-		CBufImpl *b = construct( CBufKey() );
-		if ( ! b )
-			throw InternalError("Unable to allocate Buffer");
-		BufImpl rval = BufImpl( b, DTOR(this) );
-		rval->setSelf( rval );
-		return rval;
-	}
-};
-
-void DTOR::operator()(CBufImpl *o)
-{
-	fl_->destruct( o );
-}
-
-static CBufFreeList fl;
+static CFreeList<CBufImpl> fl;
 
 BufImpl CBufImpl::getBuf(size_t capa)
 {
 	if ( capa > sizeof(data_) ) {
 		throw InternalError("ATM all buffers are std. MTU size");
 	}
-	return fl.getBuf();
+	return fl.alloc();
 }
 
 Buf IBuf::getBuf(size_t capa)
---
+++ cpsw_freelist.h
@@ -0,0 +1,158 @@
+#ifndef CPSW_FREE_LIST_H
+#define CPSW_FREE_LIST_H
+
+#include <boost/weak_ptr.hpp>
+#include <boost/lockfree/stack.hpp>
+#include <boost/atomic.hpp>
+#include <boost/make_shared.hpp>
+
+#include <cpsw_api_user.h>
+#include <cpsw_error.h>
+
+// Extend boost's lockless free-list to manage objects that are
+// handed out via smart/shared pointers.
+// Also: keep statistics
+
+using boost::weak_ptr;
+using boost::static_pointer_cast;
+using boost::lockfree::detail::freelist_stack;
+using boost::atomic;
+using boost::memory_order_relaxed;
+using boost::memory_order_release;
+using boost::memory_order_acquire;
+
+template <typename T> class CFreeList;
+
+template <typename T> class CFreeListNodeKey {
+private:
+	CFreeListNodeKey() {};
+	friend class CFreeList<T>;
+};
+
+template <typename C> class CFreeListNode {
+private:
+	weak_ptr<C> self_;
+
+protected:
+	virtual void setSelf(shared_ptr<C> me)
+	{
+		if ( ! self_.expired() || me.get() != this )
+			throw InternalError("Self pointer already set or not matching 'this'");
+		self_ = me;
+	}
+
+	friend shared_ptr<C> CFreeList<C>::alloc();
+
+public:
+	virtual shared_ptr<C> getSelf()
+	{
+		return shared_ptr<C>( self_ );
+	}
+
+
+	virtual ~CFreeListNode() {}
+};
+
+template <typename C> class CFreeListNodeAlloc : public std::allocator<C> {
+private:
+	atomic<unsigned int> *nAlloc_p_;
+	atomic<unsigned int> *nFree_p_;
+public:
+	CFreeListNodeAlloc( atomic<unsigned int> *nAlloc_p, atomic<unsigned int> *nFree_p )
+	:nAlloc_p_(nAlloc_p), nFree_p_(nFree_p)
+	{
+	}
+
+	C *allocate(size_t n)
+	{
+		nAlloc_p_->fetch_add(n, memory_order_release);
+#ifndef BOOST_LOCKFREE_FREELIST_INIT_RUNS_DTOR
+		nFree_p_->fetch_add(n, memory_order_release);
+#endif
+		return std::allocator<C>::allocate( n );
+	}
+};
+
+// NODE must be derived from CFreeListNode !
+template <typename NODE>
+class CFreeList : public freelist_stack< NODE, CFreeListNodeAlloc<NODE> >
+{
+private:
+	atomic<unsigned int> nAlloc_;
+	atomic<unsigned int> nFree_;
+
+protected:
+	class DTOR {
+		private:
+			CFreeList *fl_;
+
+			DTOR(CFreeList *fl)
+			: fl_ (fl)
+			{
+			}
+
+			friend class CFreeList;
+
+		public:
+			void operator()(NODE *o)
+			{
+				fl_->destruct( o );
+			}
+	};
+
+
+public:
+	typedef CFreeListNodeAlloc<NODE> ALLOC;
+	typedef freelist_stack< NODE, ALLOC > BASE;
+	CFreeList(int n = 0)
+	:BASE( ALLOC( (nAlloc_=0, &nAlloc_ ), (nFree_ = 0, &nFree_) ) , n)
+	{}
+
+	unsigned int getNumAlloced()
+	{
+		return nAlloc_.load( memory_order_acquire );
+	}
+
+	unsigned int getNumFree()
+	{
+		return nFree_.load( memory_order_acquire );
+	}
+
+	unsigned int getNumInUse()
+	{
+		// can't atomically read both -- since this is a diagnostic
+		// we don't care about marginal race conditions here.
+		unsigned int rval = getNumAlloced();
+		rval -= getNumFree();
+		return rval;
+	}
+
+	NODE *construct(CFreeListNodeKey<NODE> k)
+	{
+		const bool ThreadSafe = true;
+		const bool Bounded    = false;
+		NODE *rval = BASE::template construct<ThreadSafe,Bounded>( k );
+		if ( rval )
+			nFree_.fetch_sub( 1, memory_order_release );
+		return rval;
+	}
+
+	void destruct(NODE *o)
+	{
+		const bool ThreadSafe = true;
+		nFree_.fetch_add( 1, memory_order_release );
+		BASE::template destruct<ThreadSafe>( o );
+	}
+
+	shared_ptr<NODE> alloc()
+	{
+		NODE *b = construct( CFreeListNodeKey<NODE>() );
+		if ( ! b )
+			throw InternalError("Unable to allocate Buffer");
+		shared_ptr<NODE> rval = shared_ptr<NODE>( b, DTOR(this) );
+		rval->setSelf( rval );
+		return rval;
+	}
+};
+
+#endif
