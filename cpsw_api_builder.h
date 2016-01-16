e2ae6806732d7e8993c062aae5a090d0389a34e6
till straumann <till.straumann@alumni.tu-berlin.de>
2016-01-16 10:55:38 -0600

    - increased default timeout
       since we now run through our threaded UDP RX module there
       is a bit more latency...

--- cpsw_api_builder.h
+++ cpsw_api_builder.h
@@ -98,7 +98,7 @@ class INoSsiDev : public virtual IDev {
 public:
 	typedef enum ProtocolVersion { SRP_UDP_V1 = 1, SRP_UDP_V2 = 2 } ProtocolVersion;
 
-	virtual void addAtAddress(Field child, ProtocolVersion version, unsigned dport, unsigned timeoutUs = 200, unsigned retryCnt = 5, uint8_t vc = 0) = 0;
+	virtual void addAtAddress(Field child, ProtocolVersion version, unsigned dport, unsigned timeoutUs = 1000, unsigned retryCnt = 5, uint8_t vc = 0) = 0;
 	virtual void addAtStream(Field child, unsigned dport, unsigned timeoutUs, unsigned outQDepth = 16, unsigned ldFrameWinSize = 4, unsigned ldFragWinSize = 4) = 0;
 
 	virtual const char *getIpAddressString() const = 0;
