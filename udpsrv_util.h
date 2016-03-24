5501b1e1e4f7679ad4f55efc44ddc841e61b82bd
till straumann <till.straumann@alumni.tu-berlin.de>
2016-03-24 11:08:07 -0600

    - let udpsrv_util.h use cpsw_mutex.h instead of redefining CMtx

--- udpsrv_util.h
+++ udpsrv_util.h
@@ -17,75 +17,13 @@
 
 #include <cpsw_event.h>
 #include <cpsw_buf.h>
+#include <cpsw_mutex.h>
 
 #include <stdio.h>
 
 
 using boost::shared_ptr;
 using boost::atomic;
-
-class CMtx {
-	pthread_mutex_t m_;
-	const char *nam_;
-
-private:
-	CMtx(const CMtx &);
-	CMtx & operator=(const CMtx&);
-public:
-
-	class lg {
-		private:
-			CMtx *mr_;
-
-			lg operator=(const lg&);
-
-			lg(const lg&);
-
-		public:
-			lg(CMtx *mr)
-			: mr_(mr)
-			{
-				mr_->l();
-			}
-
-			~lg()
-			{
-				mr_->u();
-			}
-
-		friend class CMtx;
-	};
-
-
-	CMtx(const char *nam):nam_(nam)
-	{
-		if ( pthread_mutex_init( &m_, NULL ) ) {
-			throw InternalError("Unable to create mutex", errno);
-		}
-	}
-
-	~CMtx()
-	{
-		pthread_mutex_destroy( &m_ );
-	}
-
-	void l() 
-	{
-		if ( pthread_mutex_lock( &m_ ) ) {
-			throw InternalError("Unable to lock mutex");
-		}
-	}
-
-	void u()
-	{
-		if ( pthread_mutex_unlock( &m_ ) ) {
-			throw InternalError("Unable to unlock mutex");
-		}
-	}
-
-	pthread_mutex_t *getp() { return &m_; }
-};
-
 
 class IProtoPort;
 typedef shared_ptr<IProtoPort> ProtoPort;
