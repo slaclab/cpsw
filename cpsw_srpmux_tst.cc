02d81b8f4492112cec0ee8205f09e7da218e3fef
till straumann <till.straumann@alumni.tu-berlin.de>
2016-09-01 19:13:42 -0700

    - added copyright blob and LICENSE.txt

--- cpsw_srpmux_tst.cc
+++ cpsw_srpmux_tst.cc
@@ -1,3 +1,12 @@
+ //@C Copyright Notice
+ //@C ================
+ //@C This file is part of CPSW. It is subject to the license terms in the LICENSE.txt
+ //@C file found in the top-level directory of this distribution and at
+ //@C https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ //@C
+ //@C No part of CPSW, including this file, may be copied, modified, propagated, or
+ //@C distributed except according to the terms contained in the LICENSE.txt file.
+
 #include <cpsw_api_builder.h>
 #include <cpsw_mutex.h>
 
