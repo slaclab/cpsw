1bd50d79573babc734382501add3cdbaa3c4b41f
till straumann <till.straumann@alumni.tu-berlin.de>
2017-03-06 15:15:47 -0800

    - restructured cpsw_async_io.h -- let user supply transactions instead of callbacks

--- cpsw_async_io.cc
+++ cpsw_async_io.cc
@@ -9,80 +9,59 @@
 
 using boost::make_shared;
 
-// Helper class -- a transaction object
-//
-// The manager will keep a linked list of pending transactions
-//
-class CTransaction {
-private:
-	class CTransaction             *next_, *prev_;
-	IAsyncIOTransactionManager::TID tid_;
-	CTimeout                        timeout_;
-	IAsyncIOCallback               *completion_;
-
-	void addAfter(CTransaction *node)
+class CListHead : public CAsyncIOTransaction {
+public:
+	CListHead(const CAsyncIOTransactionKey &k)
+	: CAsyncIOTransaction( AsyncIOTransactionPool(), k )
 	{
-		next_              = node->next_;
-		node->next_->prev_ = this;
-		node->next_        = this;
-		prev_              = node;
 	}
 
-	void addBefore(CTransaction *node)
-	{
-		prev_              = node->prev_;
-		node->prev_->next_ = this;
-		node->prev_        = this;
-		next_              = node;
-	}
-
-
-	void remove ()
-	{
-		next_->prev_ = prev_;
-		prev_->next_ = next_;
-	}
-
-	friend class CAsyncIOTransactionManager;
+	virtual void free()                { }
+	virtual void complete(BufChain bc) { }
 };
+
+// call 'complete' in an exception-safe way
+class Completer {
+private:
+	AsyncIOTransaction xact_;
+	Completer(const Completer&);
+	Completer operator=(const Completer&);
+public:
+	Completer(AsyncIOTransaction xact)
+	: xact_(xact)
+	{
+	}
+
+	void operator()(BufChain bc)
+	{
+		xact_->complete(bc);
+	}
+
+	~Completer()
+	{
+		xact_->free();
+	}
+};
+
 
 class CAsyncIOTransactionManager : public CRunnable, public IAsyncIOTransactionManager {
 private:
-	CMtx          pendingMtx_;  // protect pendingList_
-	CTransaction  pendingList_; // list head + tail of pending transactions
+	CMtx                 pendingMtx_;  // protect pendingList_
+	CListHead            pendingList_; // list head + tail of pending transactions
 
-	CCond         havePending_; // condvar to signal that there are new transactions
+	CCond                havePending_; // condvar to signal that there are new transactions
 	                            // wakes up the timeout thread
 
-	CMtx          freeMtx_;     // protect free-list
-	CTransaction *freeList_;    // free-list for transactions
+	CTimeout             timeout_;
 
-	CTimeout      timeout_;
-
-	void addHead_unl(CTransaction *node)
+	void addHead_unl(AsyncIOTransaction node)
 	{
 		node->addAfter( &pendingList_ );
 	}
 
-	void addTail_unl(CTransaction *node)
+	void addTail_unl(AsyncIOTransaction node)
 	{
 		node->addBefore( &pendingList_ );
-	}
-
-	CTransaction *getXact()
-	{
-	CMtx::lg guard( &freeMtx_ );
-	CTransaction *xact;
-		if ( (xact = freeList_) )
-			freeList_ = xact->next_;
-		return xact;
-	}
-
-	void freeXact(CTransaction *xact)
-	{
-	CMtx::lg guard( &freeMtx_ );
-		xact->next_ = freeList_;
-		freeList_   = xact;
 	}
 
 public:
@@ -92,21 +71,21 @@ public:
 	bool
 	pendingListEmpty()
 	{
-		return pendingList_.next_ == &pendingList_;
+		return pendingList_.isLonely();
 	}
 
 
-	CTransaction *
+	AsyncIOTransaction 
 	getHead_unl()
 	{
 		return pendingListEmpty() ?  0 : pendingList_.next_;
 	}
 
 	virtual void
-	post(IAsyncIOCallback *cb, TID tid);
+	post(AsyncIOTransaction xact, TID tid);
 
-	virtual IAsyncIOCallback *
-	cancel(TID tid);
+	virtual int
+	complete(BufChain bc, TID tid);
 
 
 	virtual void *
@@ -117,7 +96,7 @@ public:
 
 CAsyncIOTransactionManager::CAsyncIOTransactionManager(uint64_t timeoutUs)
 : CRunnable("AsyncIOTimeout"),
-  freeList_(0),
+  pendingList_( CAsyncIOTransactionKey() ),
   timeout_ (timeoutUs)
 {
 		pendingList_.next_ = &pendingList_;
@@ -126,14 +105,9 @@ CAsyncIOTransactionManager::CAsyncIOTransactionManager(uint64_t timeoutUs)
 }
 
 void
-CAsyncIOTransactionManager::post(IAsyncIOCallback *cb, TID tid)
+CAsyncIOTransactionManager::post(AsyncIOTransaction xact, TID tid)
 {
-	CTransaction *xact = getXact();
-	if ( ! xact ) {
-		xact = new CTransaction();
-	}
 	xact->tid_        = tid;
-	xact->completion_ = cb;
 
 	clock_gettime( CLOCK_MONOTONIC, &xact->timeout_.tv_ );
 
@@ -151,11 +125,10 @@ CAsyncIOTransactionManager::post(IAsyncIOCallback *cb, TID tid)
 	}
 }
 
-IAsyncIOCallback *
-CAsyncIOTransactionManager::cancel(TID tid)
+int
+CAsyncIOTransactionManager::complete(BufChain bc, TID tid)
 {
-	CTransaction     *xact;
-	IAsyncIOCallback *rval;
+	AsyncIOTransaction xact;
 
 	// access of protected 'pendingList'
 	{
@@ -166,23 +139,21 @@ CAsyncIOTransactionManager::cancel(TID tid)
 		}
 		if ( xact == &pendingList_ ) {
 			/* not found! */
-			return 0;
+			return -1;
 		}
 		xact->remove();
 	}
 
-	rval = xact->completion_;
+	(Completer (xact))(bc);
 
-	freeXact( xact );
-
-	return rval;
+	return 0;
 }
 
 
 void *
 CAsyncIOTransactionManager::threadBody()
 {
-	CTransaction *xact;
+	AsyncIOTransaction xact;
 	CTimeout      now;
 	CTimeout      remaining;
 	bool          err = false;
@@ -218,9 +189,7 @@ bail:
 		// pendingMtx_ released at this point
 
 		if ( xact ) {
-			xact->completion_->complete(BufChain());
-
-			freeXact( xact );
+			(Completer(xact))( BufChain() );
 		} else {
 			// if pthread_cond_wait incurred an error then 'xact == 0' since
 			// otherwise we wouldn't have waited in the first place
@@ -235,7 +204,7 @@ bail:
 
 CAsyncIOTransactionManager::~CAsyncIOTransactionManager()
 {
-CTransaction *xact;
+AsyncIOTransaction xact;
 
 	threadStop();
 
@@ -244,17 +213,11 @@ CTransaction *xact;
 	while ( (xact = getHead_unl()) ) {
 		xact->remove();
 		pendingMtx_.u();
-		xact->completion_->complete( BufChain() );
-
-		delete xact;
+		(Completer(xact))( BufChain() );
 
 		pendingMtx_.l();
 	}
 	pendingMtx_.u();
-
-	while ( (xact = getXact()) )
-		delete xact;
-
 }
 
 AsyncIOTransactionManager
@@ -262,3 +225,41 @@ IAsyncIOTransactionManager::create(uint64_t timeoutUs)
 {
 	return make_shared<CAsyncIOTransactionManager>( timeoutUs );
 }
+
+CAsyncIOTransactionPoolBase::CAsyncIOTransactionPoolBase()
+: freeList_(0)
+{
+}
+
+AsyncIOTransaction
+CAsyncIOTransactionPoolBase::get()
+{
+CMtx::lg           guard( &freeListMtx_ );
+AsyncIOTransaction xact;
+	if ( (xact = freeList_) ) {
+		freeList_ = freeList_->next_;
+	}
+	return xact;
+}
+
+void
+CAsyncIOTransactionPoolBase::put(AsyncIOTransaction xact)
+{
+CMtx::lg guard( &freeListMtx_ );
+	xact->next_ = freeList_;
+	freeList_   = xact;
+}
+
+
+CAsyncIOTransactionPoolBase::~CAsyncIOTransactionPoolBase()
+{
+CMtx::lg           guard( &freeListMtx_ );
+AsyncIOTransaction xact;
+	while ( (xact = freeList_) ) {
+		freeList_ = freeList_->next_;
+		delete xact;
+	}
+}
+
+
+
--- cpsw_async_io.h
+++ cpsw_async_io.h
@@ -2,14 +2,73 @@
 #define CPSW_ASYNC_TRANSACTION_H
 
 #include <cpsw_buf.h>
+#include <cpsw_mutex.h>
+#include <cpsw_shared_obj.h>
+
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 
-class IAsyncIOCallback {
+using boost::make_shared;
+
+class   CAsyncIOTransaction;
+typedef CAsyncIOTransaction *AsyncIOTransaction;
+
+class   IAsyncIOTransactionPool;
+typedef shared_ptr<IAsyncIOTransactionPool> AsyncIOTransactionPool;
+
+class IAsyncIOTransactionPool {
 public:
-	// if this is called with no rchn then a timeout occurred
-	virtual void complete(BufChain rchn) = 0;
+	virtual void put(AsyncIOTransaction) = 0;
 
-	virtual ~IAsyncIOCallback() {}
+	virtual ~IAsyncIOTransactionPool() {}
+};
+
+class CAsyncIOTransactionPoolBase : public IAsyncIOTransactionPool {
+private:
+	CMtx               freeListMtx_;
+	AsyncIOTransaction freeList_;
+	
+	CAsyncIOTransactionPoolBase( const CAsyncIOTransactionPoolBase & );
+	CAsyncIOTransactionPoolBase operator=( const CAsyncIOTransactionPoolBase & );
+protected:
+	AsyncIOTransaction get();
+	void               put(AsyncIOTransaction);
+	
+	CAsyncIOTransactionPoolBase();
+	virtual ~CAsyncIOTransactionPoolBase();
+};
+
+class CAsyncIOTransactionKey {
+private:
+	CAsyncIOTransactionKey(){}
+	CAsyncIOTransactionKey(const CAsyncIOTransactionKey &);
+	CAsyncIOTransactionKey operator=(const CAsyncIOTransactionKey &);
+	template<class EL> friend class CAsyncIOTransactionPool;
+	friend class CAsyncIOTransactionManager;
+};
+
+template <typename EL> class CAsyncIOTransactionPool : public CAsyncIOTransactionPoolBase, public CShObj {
+public:
+	CAsyncIOTransactionPool(Key &k)
+	: CShObj(k)
+	{
+	}
+
+	typedef shared_ptr< CAsyncIOTransactionPool<EL> > SP;
+
+	EL *getTransaction()
+	{
+	EL *xact;
+		if ( (xact = static_cast<EL*>( get() )) ) {
+			return xact;
+		}
+		return new EL( getSelfAs< SP >(), CAsyncIOTransactionKey() );
+	}
+
+	static SP create()
+	{
+		return CShObj::create<SP>();
+	}
 };
 
 class IAsyncIOTransactionManager;
@@ -19,12 +78,82 @@ class IAsyncIOTransactionManager {
 public:
 	typedef uint32_t TID;	
 
-	virtual void              post(IAsyncIOCallback *, TID) = 0;
-	virtual IAsyncIOCallback *cancel(TID tid)               = 0;
+	virtual void post(AsyncIOTransaction, TID) = 0;
+	virtual int  complete(BufChain, TID)       = 0;
 
 	virtual ~IAsyncIOTransactionManager() {}
 
 	static AsyncIOTransactionManager create(uint64_t timeoutUs = 500000);
 };
+
+// A transaction object
+//
+class CAsyncIOTransaction {
+private:
+	AsyncIOTransaction              next_, prev_;
+	IAsyncIOTransactionManager::TID tid_;
+	CTimeout                        timeout_;
+	AsyncIOTransactionPool          pool_;
+
+	CAsyncIOTransaction(const CAsyncIOTransaction &);
+	CAsyncIOTransaction & operator=(const CAsyncIOTransaction &);
+
+	void addAfter(AsyncIOTransaction node)
+	{
+		next_              = node->next_;
+		node->next_->prev_ = this;
+		node->next_        = this;
+		prev_              = node;
+	}
+
+	void addBefore(AsyncIOTransaction node)
+	{
+		prev_              = node->prev_;
+		node->prev_->next_ = this;
+		node->prev_        = this;
+		next_              = node;
+	}
+
+	bool isLonely() const
+	{
+		return next_ == this;
+	}
+
+
+	void remove ()
+	{
+		next_->prev_ = prev_;
+		prev_->next_ = next_;
+	}
+
+protected:
+	CAsyncIOTransaction( AsyncIOTransactionPool pool, const CAsyncIOTransactionKey &key )
+	: next_( this ),
+	  prev_( this ),
+	  pool_(pool)
+	{
+	}
+
+	virtual ~CAsyncIOTransaction()
+	{
+	}
+	
+public:
+
+	// to be overridden by concrete transaction classes
+	virtual void complete(BufChain) = 0;
+
+	virtual void free()
+	{
+		pool_->put( this );
+	}
+
+
+
+	friend class CAsyncIOTransactionManager;
+	friend class CAsyncIOTransactionPoolBase;
+};
+
+
 
 #endif
