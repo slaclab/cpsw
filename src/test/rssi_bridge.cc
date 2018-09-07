6024b8ee8163e0b5231d8500071b94b379447e90
till straumann <till.straumann@alumni.tu-berlin.de>
2018-09-06 19:31:42 -0700

    rssi_bridge: enhancements: -u, -C; dynamic connection management

--- src/test/rssi_bridge.cc
+++ src/test/rssi_bridge.cc
@@ -15,6 +15,7 @@
 #include <getopt.h>
 #include <string.h>
 #include <vector>
+#include <stdint.h>
 
 #include <arpa/inet.h>
 #include <boost/make_shared.hpp>
@@ -42,7 +43,7 @@ public:
 
 typedef shared_ptr<CMover> Mover;
 
-class Bridge {
+class Bridge : public ITcpConnHandler {
 private:
 	Mover     udpToTcp_;
 	Mover     tcpToUdp_;
@@ -51,7 +52,7 @@ private:
 	unsigned  flags_;
 
 public:
-	typedef enum { NONE = 0, RSSI=1, DEBUG=2 } Flags;
+	typedef enum { NONE = 0, RSSI=1, DEBUG=2, ALWAYSCONN=4 } Flags;
 
 	Bridge(const char *peerIp, unsigned short peerPort, unsigned short myPort, Flags flags);
 
@@ -65,6 +66,9 @@ public:
 	{
 		return !!(flags_ & DEBUG);
 	}
+
+	virtual void up();
+	virtual void down();
 
 	void shutdown();
 
@@ -110,8 +114,10 @@ CMover::~CMover()
 Bridge::Bridge(const char *peerIp, unsigned short peerPort, unsigned short myPort, Flags flags)
 : flags_(flags)
 {
+TcpConnHandler connHdlr = (flags & Bridge::ALWAYSCONN) ? TcpConnHandler() : this;
+
 UdpPort   udpPrt  = IUdpPort::create( NULL, 0, 0, 0 );
-TcpPort   tcpPrt  = ITcpPort::create( NULL, myPort );
+TcpPort   tcpPrt  = ITcpPort::create( NULL, myPort, connHdlr );
 
 		if ( useRssi() ) {
 			udpTop_ = CRssiPort::create( false );
@@ -124,10 +130,12 @@ TcpPort   tcpPrt  = ITcpPort::create( NULL, myPort );
 
 		udpPrt->connect( peerIp, peerPort );
 
-		for ( ProtoPort p=udpTop_; p; p=p->getUpstreamPort() )
-			p->start();
 		for ( ProtoPort p=tcpTop_; p; p=p->getUpstreamPort() )
 			p->start();
+
+		if ( ! connHdlr ) {
+			up();
+		}
 
 		try {
 
@@ -142,6 +150,28 @@ TcpPort   tcpPrt  = ITcpPort::create( NULL, myPort );
 			throw;
 		}
 };
+
+// Implement a TCP connection handler so that we attempt to connect the 
+// UDP side only when contacted from TCP. This way we can always start
+// the bridge on a standard set of ports and leave it to the remote peer
+// to talk to the ones where something is actually listening on the UDP
+// side.
+
+void
+Bridge::up()
+{
+	// Start up UDP side
+	for ( ProtoPort p=udpTop_; p; p=p->getUpstreamPort() )
+		p->start();
+}
+
+void
+Bridge::down()
+{
+	// Shut down UDP side
+	for ( ProtoPort p=udpTop_; p; p=p->getUpstreamPort() )
+		p->stop();
+}
 
 void
 Bridge::shutdown()
@@ -164,7 +194,7 @@ Bridge::~Bridge()
 static void
 usage(const char *nm)
 {
-	fprintf(stderr,"Usage: %s [-hrd] -a <dest_ip> [-p <dst_port>[:<tcp_srv_port>]]\n", nm);
+	fprintf(stderr,"Usage: %s [-hdC] -a <dest_ip> [-p <dst_port>[:<tcp_srv_port>]] [-u <dst_port>[:<tcp_srv_port>]]\n", nm);
 	fprintf(stderr,"       RSSI <-> TCP bridge\n");
 	fprintf(stderr,"       -a <dest_ip>              : remote address where a RSSI server is listening.\n");
 	fprintf(stderr,"       -p <dst_port>[:<tcp_port>]: <dst_port> identifies the remote UDP port and <tcp_port>\n");
@@ -172,13 +202,21 @@ usage(const char *nm)
 	fprintf(stderr,"                                   defaults to <dst_port>.\n");
 	fprintf(stderr,"                                   Note that multiple -p options may be given in order to\n");
 	fprintf(stderr,"                                   bridge multiple connections.\n");
+	fprintf(stderr,"       -u <dst_port>[:<tcp_port>]: bridge pure UDP connection **without RSSI**\n");
+	fprintf(stderr,"                                   <dst_port> identifies the remote UDP port and <tcp_port>\n");
+	fprintf(stderr,"                                   the local TCP port where this program is listening. It\n");
+	fprintf(stderr,"                                   defaults to <dst_port>.\n");
+	fprintf(stderr,"                                   Note that multiple -u options may be given in order to\n");
+	fprintf(stderr,"                                   bridge multiple connections.\n");
 	fprintf(stderr,"       -h                        : This message.\n");
-	fprintf(stderr,"       -r                        : Disable RSSI. Use pure UDP.\n");
 #ifdef RSSI_BR_DEBUG
 	fprintf(stderr,"       -d                        : Enable debug messages\n");
 #else
 	fprintf(stderr,"       -d                        : Debug support not compiled\n");
 #endif
+	fprintf(stderr,"       -C                        : Always connect UDP side. If this is omitted\n");
+	fprintf(stderr,"                                   then connections on the UDP side are only\n");
+	fprintf(stderr,"                                   initiated when a TCP connection is accepted\n");
 }
 
 #define DFLT_PORT 8192
@@ -191,11 +229,13 @@ main(int argc, char **argv)
 const char    *peerIp      = "127.0.0.1";
 int            rval        = 1;
 int            opt;
-Bridge::Flags  flags       = Bridge::RSSI;
+Bridge::Flags  flags       = Bridge::NONE;
+uint64_t       rssiMsk     = 0;
+uint64_t       rssiBit     = 1;
 
 std::vector< PP > ports;
 
-	while ( (opt = getopt(argc, argv, "hp:a:dr")) > 0 ) {
+	while ( (opt = getopt(argc, argv, "hCp:u:a:d")) > 0 ) {
 		switch (opt) {
 			case 'h':
 				rval = 0;
@@ -203,8 +243,19 @@ std::vector< PP > ports;
 				usage( argv[0] );
 				return rval;
 			case 'p':
+				rssiMsk |= rssiBit;
+				// fall through
+			case 'u':
 				unsigned short me, peer;
 				int st;
+
+				if ( 0 == rssiBit ) {
+					fprintf(stderr,"Too many -u/-p options; only %d supported\n", (unsigned)sizeof(rssiBit)*8);
+					return rval;
+				}
+
+				rssiBit <<= 1;
+
 				if ( ::strchr(optarg,':') ) {
 					st = (2 != sscanf(optarg, "%hi:%hi", &peer, &me) );
 				} else {
@@ -228,8 +279,8 @@ std::vector< PP > ports;
 				flags = (Bridge::Flags)(flags | Bridge::DEBUG);
 				break;
 
-			case 'r':
-				flags = (Bridge::Flags)(flags & ~Bridge::RSSI);
+			case 'C':
+				flags = (Bridge::Flags)(flags | Bridge::ALWAYSCONN);
 				break;
 
 			case 'a':
@@ -249,8 +300,15 @@ std::vector< PP > ports;
 try {
 	std::vector< shared_ptr<Bridge> > bridges;
 
+	rssiBit = 1;
+
 	for ( std::vector<PP>::const_iterator it = ports.begin(); it != ports.end(); ++it ) {
-		bridges.push_back( make_shared<Bridge>( peerIp, it->first, it->second, flags ) );
+		Bridge::Flags f = flags;
+		if ( (rssiBit & rssiMsk) ) {
+			f = (Bridge::Flags)(f | Bridge::RSSI);
+		}
+		bridges.push_back( make_shared<Bridge>( peerIp, it->first, it->second, f ) );
+		rssiBit <<= 1;
 	}
 
 	pause();
